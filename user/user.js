bf90369f6a8fb28390cfaecfdf0d1a19a274f94d
LiBenn349 <147031832+LiBenn349@users.noreply.github.com>
2025-06-26 03:35:33 +0200

    Update user.js

--- user/user.js
+++ user/user.js
@@ -1,11 +1,46 @@
-require('dotenv').config();
+//Installierte Libraries express dotenv cors mongoose node-fetch jsonwebtoken
 const express = require('express');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
-const authRoutes = require('./routes/auth');
 
+const port = process.env.PORT;
+const secret = process.env.SECRET;
+const dburl = process.env.DBURL;
+
+//Initialisierung Mongoose
+const mongoose = require('mongoose');
+
+//Initialisierung CORS
+const cors = require('cors');
+
+//Express soll JSON verwenden
 app.use(express.json());
-app.use('/auth', authRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Auth-Service running on port ${process.env.PORT}`);
-});
+//Server soll Cross Site Scripting Kompatibel sein
+app.use(cors());
+
+//Datenbankverbindung aufbauen
+mongoose.connect(dburl); //Baut Verbindung zur DB auf, die letztlich in der .env Datei hinterlegt ist
+const db = mongoose.connection; //Connection in der DB Konstante verfügbar machen
+db.on('error', (error) => console.error(error));//Falls ein Fehler bei der Verbindung auftritt, soll der Fehler in der Server console ausgegeben werden
+db.once('open',() => console.log("Server hat Verbindung zur DB " + dburl + " erfolgreich hergestellt")); //Öffnen der DB Verbindung und Ausgabe der Meldung, dass die Verbindung hergestellt wurde wenn alles erfolgreich ist
+
+
+//Berechtigte User
+const users = [
+    {
+        username: "test",
+        password: "test2"
+    },{
+        username: "zweiterUser",
+        password: "hallo" 
+    }
+]
+
+const router = require('./routes/userRouter');
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log("Server gestartet auf Port: " + port);
+})
